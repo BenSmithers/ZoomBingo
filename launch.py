b07aeb604200c58f4073532f7373620a30394170
Boggle-Head <b.smithers.actual@gmail.com>
2020-05-08 15:41:16 -0500

    docstrings, better support for arbitrary sized Bingo boards

--- launch.py
+++ launch.py
@@ -1,19 +1,32 @@
-import sys
-import os
-from random import randint
+import sys # passing args. Not really used atm
+import os # used to get the datafile's location 
+from random import randint # randomly choose text
 
-from board import Ui_MainWindow
+from board import Ui_MainWindow # UI
 
-from PyQt5 import QtCore, QtGui
-from PyQt5.QtWidgets import QMainWindow, QWidget, QApplication
+# various gui elements
+from PyQt5.QtWidgets import QMainWindow, QWidget, QApplication, QPushButton
 
+'''
+Ben Smithers (benjamin.smithers@mavs.uta.edu)
+
+~~~ I regret nothing ~~~
+
+07 May 2020 
+'''
+
+
+# which phrases file to use
 word_file = os.path.join(os.path.dirname(__file__), "phrases.dat")
 
-# this returns a function that disables the button at (x,y) 
-# needs to be outside the main class for proper scoping of the "disable" function 
-def get_disable_function( what, x, y):
+def get_disable_function( button ):
+    """
+    Returns a function that disables the given button
+    """
+    if not isinstance(button, QPushButton):
+        raise TypeError("Arg should be {}, not {}".format(QPushButton, type(button)))
     def disable():
-        what.ui.buttons[x][y].setEnabled(False)
+        button.setEnabled(False)
     return( disable )
 
 class main_gui(QMainWindow):
@@ -26,21 +39,34 @@ class main_gui(QMainWindow):
         self.phrases = file_obj.readlines()
         file_obj.close()
         print("Loaded {} phrases".format(len(self.phrases)))
-        if len(self.phrases)<24:
+        
+        # assure we have enough phrases to make the bingo board
+        if len(self.phrases)<(self.ui.x_dim*self.ui.y_dim):
             raise Exception("Insufficient phrases! {}".format(len(self.phrases)))
 
+        # need to assign starting values to the buttons
         self.assign()
 
-        # Buttons simply disable themselves when pressed         
+        # Need to assign a function to each button
+        # buttons disable themselves when pressed. 
         for x in range(self.ui.x_dim):
             for y in range(self.ui.y_dim):
-                self.ui.buttons[x][y].clicked.connect(get_disable_function(self,x,y) )
+                self.ui.buttons[x][y].clicked.connect(get_disable_function(self.ui.buttons[x][y]))
 
+        # assign function to reset button 
         self.ui.pushButton.clicked.connect(self.assign)
 
     def add_line_breaks(self, what):
+        """
+        Really simplistic method for adding line breaks to the phrases
+
+        Returns passed string with linebreaks inserted 
+        """
+        if not isinstance(what, str):
+            raise TypeError("Arg 'what' should be {}, got {}".format(str, type(what)))
+
         working = what.split(" ")
-        freq = 2
+        freq = 2 # number of words before a linebreak 
         n_linebreaks = int(len(working)/freq)
 
         for i in range(n_linebreaks):
@@ -51,12 +77,30 @@ class main_gui(QMainWindow):
     
 
     def assign(self):
+        """
+        Re-enables all the buttons and assigns new text to them. 
+        """
+
         # make a new list of phrases from which to sample
         phrase_copy = [phrase for phrase in self.phrases]
 
+        # get the coordinates of the Free space
+        #    if even, no free space 
+        if x_dim%2 == 0:
+            middle_x = -1
+            # setting this to -1 so that no coordinate will match it, and so there is not free space 
+        else:
+            middle_x = (x_dim-1)/2
+        if y_dim%2 ==0:
+            middle_y = -1
+        else:
+            middle_y = (y_dim-1)/2
+
+
         for x in range(self.ui.x_dim):
             for y in range(self.ui.y_dim):
-                if x==2 and y==2:
+                if x==middle_x and y==middle_y:
+                    # skip the free space 
                     self.ui.buttons[x][y].setText("Free \n Space!")
                 else:
                     if len(phrase_copy)==1:
@@ -64,14 +108,16 @@ class main_gui(QMainWindow):
                     else:
                         which = randint( 0, len(phrase_copy)-1)
                     self.ui.buttons[x][y].setText( self.add_line_breaks(phrase_copy[which]) ) 
+
+                    # make sure no phrase is assigned twice! 
                     phrase_copy.pop(which)
 
                     self.ui.buttons[x][y].setEnabled(True)
 
 
+# instantiate and run the app
 app = QApplication(sys.argv)
 app_instance = main_gui()
-
 if __name__=="__main__":
     app_instance.show()
     sys.exit(app.exec_())
